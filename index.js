4ab2ff198a6cf347e7f91584c70cbd6ed17dcfe4
Toni Penya-Alba <toni.penya.alba@gmail.com>
2018-09-22 22:49:13 +0200

    Add start/stop functionality

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 const {ipcRenderer, shell} = require('electron')
 
+document.addEventListener('DOMContentLoaded', init)
+
+let playButton
+let pauseButton
 let timeLeft = 10000
 const UPDATE_INTERVAL = 500
+var timerIntervalId
+var running = false
 
-var timerIntervalId = setInterval(() => {
-  timeLeft -= UPDATE_INTERVAL
+function init() {
+  playButton = document.getElementById('play')
+  stopButton = document.getElementById('stop')
+  playButton.addEventListener('click', startTimer)
+  stopButton.addEventListener('click', stopTimer)
 
+  stopTimer()
+}
+
+function startTimer() {
+  timerIntervalId = setInterval(() => {
+    timeLeft -= UPDATE_INTERVAL
+
+    if (timeLeft <= 0) {
+      clearInterval(timerIntervalId)
+    }
+
+    running = true
+    updateInterface()
+  }, UPDATE_INTERVAL)
+  updateInterface()
+}
+
+function stopTimer() {
+  timeLeft = 10000
+  running = false
+  clearInterval(timerIntervalId)
+  updateInterface()
+}
+
+function updateInterface() {
   const secondsLeft = Math.trunc(timeLeft / 1000)
   ipcRenderer.send('timer-updated', secondsLeft)
   document.getElementById('countdown').innerHTML = secondsLeft
 
-  if (timeLeft <= 0) {
-    clearInterval(timerIntervalId)
+  if (running) {
+    playButton.classList.add('hidden')
+    stopButton.classList.remove('hidden')
+  } else {
+    playButton.classList.remove('hidden')
+    stopButton.classList.add('hidden')
   }
-}, UPDATE_INTERVAL)
+}
 
