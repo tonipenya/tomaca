799b5f30842c4319d345ef3a1065f8faa859c6f8
Toni Penya-Alba <toni.penya.alba@gmail.com>
2018-09-23 12:43:00 +0200

    Refactor code

--- index.js
+++ index.js
@@ -14,62 +14,61 @@ var CIRCUMFERENCE = 2 * Math.PI * RADIUS;
 let timerDuration = 10000
 let timeLeft = timerDuration
 var timerIntervalId
-var running = false
 
 function init() {
+  initAppConttrols()
+  initStartStopControls()
+  initPreferencesControls()
+  initProgressRing()
+  updateLabels()
+}
+
+function initAppConttrols(){
+  appActions = document.getElementById('app-actions')
+  document.getElementById('quit-btn').addEventListener('click', window.close)
+}
+
+function initStartStopControls() {
   playButton = document.getElementById('play')
   stopButton = document.getElementById('stop')
-  preferencesPane = document.getElementById('preferences')
-  appActions = document.getElementById('app-actions')
-  preferencesActions = document.getElementById('preferences-actions')
-  progressRing = document.querySelector('.progress-value');
   playButton.addEventListener('click', startTimer)
   stopButton.addEventListener('click', stopTimer)
-  document.getElementById('quit-btn').addEventListener('click', window.close)
-  document.getElementById('preferences-btn').addEventListener('click', togglePreferences)
-  document.getElementById('preferences-cancel').addEventListener('click', togglePreferences)
-  document.getElementById('preferences-save').addEventListener('click', savePreferences)
-  progressRing.style.strokeDasharray = CIRCUMFERENCE;
-
-  stopTimer()
 }
 
 function startTimer() {
+  clearInterval(timerIntervalId)
+  playButton.classList.add('hidden')
+  stopButton.classList.remove('hidden')
+  startProgressRing()
+  updateLabels()
+
   timerIntervalId = setInterval(() => {
     timeLeft -= UPDATE_INTERVAL
+    updateLabels()
 
     if (timeLeft <= 0) {
       clearInterval(timerIntervalId)
     }
-
-    running = true
-
-    updateInterface()
   }, UPDATE_INTERVAL)
-
-  startProgressRing()
-  updateInterface()
 }
 
 function stopTimer() {
   timeLeft = timerDuration
-  running = false
+  playButton.classList.remove('hidden')
+  stopButton.classList.add('hidden')
   clearInterval(timerIntervalId)
   resetProgressRing()
-  updateInterface()
+  updateLabels()
 }
 
-function updateInterface() {
-  const secondsLeft = Math.trunc(timeLeft / 1000)
+function updateLabels() {
+  const secondsLeft = Math.round(timeLeft / 1000)
   ipcRenderer.send('timer-updated', secondsLeft)
   document.getElementById('countdown-number').innerHTML = secondsLeft
+}
 
-  if (running) {
-    playButton.classList.add('hidden')
-    stopButton.classList.remove('hidden')
-  } else {
-    playButton.classList.remove('hidden')
-    stopButton.classList.add('hidden')
-  }
+function initProgressRing() {
+  progressRing = document.querySelector('.progress-value');
+  progressRing.style.strokeDasharray = CIRCUMFERENCE;
 }
 
